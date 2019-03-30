706c8ac0499ad5e393b97ea53e715c06dafdfc7e
kranewasd <kranehecaru@gmail.com>
2019-03-29 19:38:47 -0700

    Enlarge a file
    Looking for "date.txt" file, and copies whatever is in there until you close the program.

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <fstream>
+#include <stdio.h>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+
+	
+	FILE * date, * dateout;
+	dateout = fopen("date.txt", "a");
+	date = fopen("date.txt", "r");
+	
+	
+	char c;
+	
+
+	while ( 1 ) {
+		Sleep(1 / 60);
+		//citim din fisierul date
+		fscanf(date, "%c", &c);
+
+		//scriem in fisierul date cu functia append
+		fprintf(dateout, "%c", c);
+		
+
+	}
+	
+	fclose(dateout);
+	fclose(date);
+
+	
+	return 0;
+
+
+}
